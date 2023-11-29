7459dbd366b367ee59ee2408dbe7abca9fea5dac
TosoK <tosekleckaroski@gmail.com>
2023-11-29 09:56:15 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(document.querySelector('.message'))
+
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 13
+document.querySelector('.score').textContent = 10
+document.querySelector('.guess').value = 2
