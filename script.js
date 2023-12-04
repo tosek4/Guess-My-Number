2cc6a0bc60a9971e0ab2ba54b3c96243c978ebb1
TosoK <tosekleckaroski@gmail.com>
2023-12-04 08:28:32 +0100

    feat: implement js-logic

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-console.log(document.querySelector('.message'))
+let searchNumber = Math.trunc(Math.random()*20) + 1
+let yourNumber
+let score = 20
+let highScore = 0
+document.querySelector('.check').addEventListener('click', function () {
+     yourNumber = document.querySelector('.guess').value
+    if(searchNumber == yourNumber){
 
-document.querySelector('.message').textContent = 'Correct Number!'
+        if(highScore < score){
+            highScore = score
+            document.querySelector('.highscore').textContent= highScore
+        }
+        document.querySelector('.message').textContent = 'Correct Number - You win'
+        document.querySelector('body').style.backgroundColor = 'green'
 
-document.querySelector('.number').textContent = 13
-document.querySelector('.score').textContent = 10
-document.querySelector('.guess').value = 2
+    }else if(score == 1){
+
+        document.querySelector('.message').textContent = 'End Game You lost'
+        document.querySelector('body').style.backgroundColor = 'red'
+
+    }else if(searchNumber > yourNumber){
+
+        score--
+        document.querySelector('.message').textContent = 'Too low!'
+        document.querySelector('.score').textContent = score
+
+    }else if(searchNumber < yourNumber){
+
+        score--
+        document.querySelector('.message').textContent = 'Too high!'
+        document.querySelector('.score').textContent = score
+
+    }
+})
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.score').textContent = 20
+    document.querySelector('.guess').value = null
+    score = 20
+
+})
