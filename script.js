9a97381a70107d2aa89be1a97519ea575f33fcf7
TosoK <tosekleckaroski@gmail.com>
2023-12-04 08:54:06 +0100

    ref: refactor conditions, make separate const for messages

--- script.js
+++ script.js
@@ -2,39 +2,39 @@ let searchNumber = Math.trunc(Math.random()*20) + 1
 let yourNumber
 let score = 20
 let highScore = 0
+const winMessages = 'Correct Number - You win'
+const endGameMessages = 'End Game You lost'
+const tooHighMessages = 'Too high!'
+const tooLowMessages = 'Too low!'
+const startGuessingMessages = 'Start guessing...'
+
+const displayMessages = function (message){
+    document.querySelector('.message').textContent = message
+}
 document.querySelector('.check').addEventListener('click', function () {
      yourNumber = Number(document.querySelector('.guess').value)
     if(searchNumber === yourNumber){
-
         if(highScore < score){
             highScore = score
             document.querySelector('.highscore').textContent= highScore
         }
-        document.querySelector('.message').textContent = 'Correct Number - You win'
+        displayMessages(winMessages)
         document.querySelector('body').style.backgroundColor = 'green'
 
     }else if(score === 1){
-
-        document.querySelector('.message').textContent = 'End Game You lost'
+        displayMessages(endGameMessages)
         document.querySelector('body').style.backgroundColor = 'red'
 
-    }else if(searchNumber > yourNumber){
-
+    }else if(searchNumber !== yourNumber) {
         score--
-        document.querySelector('.message').textContent = 'Too low!'
+        displayMessages(searchNumber < yourNumber ? tooHighMessages  : tooLowMessages)
         document.querySelector('.score').textContent = score
-
-    }else if(searchNumber < yourNumber){
-
-        score--
-        document.querySelector('.message').textContent = 'Too high!'
-        document.querySelector('.score').textContent = score
-
     }
 })
 document.querySelector('.again').addEventListener('click', function () {
+    searchNumber = Math.trunc(Math.random()*20) + 1
     document.querySelector('body').style.backgroundColor = '#222'
-    document.querySelector('.message').textContent = 'Start guessing...'
+    displayMessages(startGuessingMessages)
     document.querySelector('.number').textContent = '?'
     document.querySelector('.score').textContent = 20
     document.querySelector('.guess').value = null
